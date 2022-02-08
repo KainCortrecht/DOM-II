abeabfbe91f1a7e770b6b82359f4fb715e86e752
Kain Cortrecht <kaincortrecht@Kains-MacBook-Air.local>
2022-02-08 17:30:01 -0500

    wip

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import './less/index.less'
 
 // Your code goes here!
+window.onload = function(evt) {
+    const heading = document.querySelector('h1')
+    heading.textContent = 'PARTY TIME!';
+}
+
