f07524fb1528e3b22ae24f0b9a2cd96070c0a995
Kain Cortrecht <kaincortrecht@Kains-MacBook-Air.local>
2022-02-08 18:07:46 -0500

    still wip

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
-import './less/index.less'
+import "./less/index.less";
 
 // Your code goes here!
-window.onload = function(evt) {
-    const heading = document.querySelector('h1')
-    heading.textContent = 'PARTY TIME!';
-}
+window.onload = function (evt) {
+  const heading = document.querySelector("h1");
+  heading.textContent = "PARTY TIME!";
 
+  window.addEventListener("copy", () => {
+      navigator.clipboard.readText()
+      .then(text => {
+          heading.textContent += text;
+      })
+  });
+
+  document.body.addEventListener('click', evt => {
+      evt.target.classList.toggle('mirror')
+  })
+};
