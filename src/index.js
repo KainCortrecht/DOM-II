1c15a10ae2db03ae1457f5e0bc566d423a4cd3a6
Kain Cortrecht <kaincortrecht@Kains-MacBook-Air.local>
2022-02-08 21:19:03 -0500

    still working

--- src/index.js
+++ src/index.js
@@ -6,23 +6,37 @@ window.onload = function (evt) {
   heading.textContent = "PARTY TIME!";
 
   window.addEventListener("copy", () => {
-      navigator.clipboard.readText()
-      .then(text => {
-          heading.textContent += text;
-      })
-  })
+    navigator.clipboard.readText().then((text) => {
+      heading.textContent += text;
+    });
+  });
 
-  document.body.addEventListener('click', evt => {
-      evt.target.classList.toggle('mirror')
-  })
+  document.body.addEventListener("click", (evt) => {
+    evt.target.classList.toggle("mirror");
+  });
 
-  document.body.addEventListener('dblclick', evt =>{
-      evt.target.outerHTML = ''
-  })
+  document.body.addEventListener("dblclick", (evt) => {
+    evt.target.outerHTML = "";
+  });
 
-  window.addEventListener('keydown', evt => {
-      if (evt.key == 6) {
-          document.body.innerHTML = '<h1>You Shall Not Pass!<h1>'
-      }
-  })
+  window.addEventListener("keydown", (evt) => {
+    if (evt.key == 6) {
+      document.body.innerHTML = "<h1>You Shall Not Pass!<h1>";
+    }
+  });
+
+  document.body.addEventListener("mousemove", (evt) => {
+    console.log(evt);
+    const { clientX, clientY } = evt;
+  });
+
+  const destinations = document.querySelectorAll(".destination");
+  for (let destination of destinations) {
+    destination.addEventListener("mouseenter", () => {
+      destination.style.color = 'pink';
+    });
+    destination.addEventListener("mouseleave", () => {
+      destination.style.color = "initial";
+    });
+  }
 };
